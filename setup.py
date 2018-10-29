189f2bd879f7ec3128e83294e131ab3ca439c7bf
Cris Mihalache <me@f3rno.com>
2018-10-29 15:00:28 +0200

    (feature) add setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='bfxhfindicators', version='1.0.0', author='Cris Mihalache',
+      author_email='cris.m@bitfinex.com',
+      url="https://github.com/bitfinexcom/bfx-hf-strategy-hf", license='LICENSE',
+      packages=['bfxhfindicators'], install_requires=[],
+      description="Trading indicators library for the Bitfinex Honey Framework")
